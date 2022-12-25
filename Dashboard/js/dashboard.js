c39cc4c344295f31c8dddc282c82402b10a39c83
Haris Mohanty <harismohanty8658@gmail.com>
2022-12-25 10:15:14 +0530

    Create Questions( Choose Subject Value )Show code fixed.

--- Dashboard/js/dashboard.js
+++ Dashboard/js/dashboard.js
@@ -50,9 +50,10 @@ const StoreSubjectBtn = () => {
   updateSubject();
 };
 
-const newSubject = (subject,index) => {         //Call the above function newSubject()
+const newSubject = (subject, index) => {
+  //Call the above function newSubject()
   let subject_name = inputSubjectValue.value;
-  if(subject){
+  if (subject) {
     subject_name = subject.subjectName;
   }
   visibleSubject.innerHTML += `
@@ -65,92 +66,126 @@ const newSubject = (subject,index) => {         //Call the above function newSub
     </div>
   </div>
   `;
-//SUBJECT "DELETE" CODE START
-let delAllBtn = visibleSubject.querySelectorAll(".del-btn");
-let i;
-for (i=0 ; i<delAllBtn.length ; i++){
-  delAllBtn[i].onclick = function(){
-    let parent = this.parentElement.parentElement;
-    //sweet alert code start
-    swal({
-      title: "Are you sure?",
-      text: "Once deleted, you will not be able to recover this imaginary file!",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((willDelete) => {
-      if (willDelete) {
-        parent.remove(); //Remove data from page but not local storage
-        updateSubject(); //Update local storage data
-        swal("Poof! Your imaginary file has been deleted!", {
-          icon: "success",
-        });
-      } else {
-        swal("Your imaginary file is safe!");
-      }
-    });
-    //sweet alert code end
+  //SUBJECT "DELETE" CODE START
+  let delAllBtn = visibleSubject.querySelectorAll(".del-btn");
+  let i;
+  for (i = 0; i < delAllBtn.length; i++) {
+    delAllBtn[i].onclick = function () {
+      let parent = this.parentElement.parentElement;
+      //sweet alert code start
+      swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      }).then((willDelete) => {
+        if (willDelete) {
+          parent.remove(); //Remove data from page but not local storage
+          updateSubject(); //Update local storage data
+          swal("Poof! Your imaginary file has been deleted!", {
+            icon: "success",
+          });
+        } else {
+          swal("Your imaginary file is safe!");
+        }
+      });
+      //sweet alert code end
+    };
   }
-}
-//SUBJECT "DELETE" CODE END
+  //SUBJECT "DELETE" CODE END
 
-//SUBJECT "EDIT" BUTTON CODE START
-let editAllBtn = visibleSubject.querySelectorAll(".edit-btn");
-for(i=0; i<editAllBtn.length; i++){
-  editAllBtn[i].onclick = function() {
-    let parent = this.parentElement.parentElement;
-    let saveBtn = parent.querySelector(".save-btn");
-    let h3 = parent.getElementsByTagName("H3");
-    h3[0].contentEditable = true;
-    h3[0].focus();
-    this.classList.add("d-none");
-    saveBtn.classList.remove("d-none");
-    saveBtn.onclick = function(){
-      let editedSub= h3[0].innerHTML;
-      let id = h3[0].getAttribute("index");
-      updateSubject(editedSub,id);
+  //SUBJECT "EDIT" BUTTON CODE START
+  let editAllBtn = visibleSubject.querySelectorAll(".edit-btn");
+  for (i = 0; i < editAllBtn.length; i++) {
+    editAllBtn[i].onclick = function () {
+      let parent = this.parentElement.parentElement;
+      let saveBtn = parent.querySelector(".save-btn");
+      let h3 = parent.getElementsByTagName("H3");
+      h3[0].contentEditable = true;
+      h3[0].focus();
       this.classList.add("d-none");
-      editAllBtn[id].classList.remove("d-none");
-      h3[0].contentEditable = false;
-    }
+      saveBtn.classList.remove("d-none");
+      saveBtn.onclick = function () {
+        let editedSub = h3[0].innerHTML;
+        let id = h3[0].getAttribute("index");
+        updateSubject(editedSub, id);
+        this.classList.add("d-none");
+        editAllBtn[id].classList.remove("d-none");
+        h3[0].contentEditable = false;
+      };
+    };
   }
-}
-//SUBJECT "EDIT" BUTTON CODE END
+  //SUBJECT "EDIT" BUTTON CODE END
 };
 
 //SUBJECT NAME GET FROM LOCAL STORAGE START
-if(localStorage.getItem(brandcode+"_allSubject") != null){
-  allSubject = JSON.parse(localStorage.getItem(brandcode+"_allSubject"));
-  allSubject.forEach((subject , index)=>{
-    newSubject(subject,index);
-  })
+if (localStorage.getItem(brandcode + "_allSubject") != null) {
+  allSubject = JSON.parse(localStorage.getItem(brandcode + "_allSubject"));
+  allSubject.forEach((subject, index) => {
+    newSubject(subject, index);
+  });
 }
 //SUBJECT NAME GET FROM LOCAL STORAGE END
 
 //UPDATE SUBJECT NAME IN LOCAL STORAGE START
-function updateSubject(subject,id){
-  if(subject != undefined && id != undefined){
+function updateSubject(subject, id) {
+  if (subject != undefined && id != undefined) {
     allSubject[id] = {
-      subjectName : subject
-    }
-  }else{
+      subjectName: subject,
+    };
+  } else {
     let subjectBox = visibleSubject.querySelectorAll(".subject-box");
-  let i;
-  allSubject = [];
-  for(i=0; i<subjectBox.length; i++){
-    let h3 = subjectBox[i].getElementsByTagName("H3");
-    allSubject.push({
-      subjectName : h3[0].innerHTML
-    });
-  }
+    let i;
+    allSubject = [];
+    for (i = 0; i < subjectBox.length; i++) {
+      let h3 = subjectBox[i].getElementsByTagName("H3");
+      allSubject.push({
+        subjectName: h3[0].innerHTML,
+      });
+    }
   }
   //SET SUBJECT IN LOCAL STORAGE
-  localStorage.setItem(brandcode+"_allSubject" , JSON.stringify(allSubject));
+  localStorage.setItem(brandcode + "_allSubject", JSON.stringify(allSubject));
 }
 //UPDATE SUBJECT NAME IN LOCAL STORAGE END
 
-
 //SUBJECT CODING END
 
+//CREATE QUESTIONS (CHOOSE SUBJECT VALUE SHOW) CODE START
+let choose_subject = document.getElementById("choose-subject");
+let question_form = document.querySelector(".question-form");
+let allQuestionInput = question_form.querySelectorAll("INPUT");
+let allQuestion = [];
+question_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  insertQuestionFunc();
+});
+const chooseSubjectFunction = () => {
+  allSubject.forEach((subject, index) => {
+    choose_subject.innerHTML += `
+    <Option>${subject.subjectName}</Option>
+    `;
+  });
+};
+chooseSubjectFunction();
 
+function insertQuestionFunc() {
+  if (choose_subject.value != "choose subject") {
+    //"choose subject" is <option> value in form
+    allQuestion.push({
+      question: allQuestionInput[0].value,
+      optionOne: allQuestionInput[1].value,
+      optionTwo: allQuestionInput[2].value,
+      optionThree: allQuestionInput[3].value,
+      optionFour: allQuestionInput[4].value,
+      optionAns: allQuestionInput[5].value,
+    });
+    localStorage.setItem(brandcode + "_" + choose_subject.value + "_question", JSON.stringify(allQuestion));
+    swal("Congratulations!", "Your Question Stored Successfully !", "success");
+    question_form.reset();
+  } else {
+    swal("Invalid Choise!", "Please select a Subject !", "warning");
+  }
+}
+//CREATE QUESTIONS (CHOOSE SUBJECT VALUE SHOW) CODE END
