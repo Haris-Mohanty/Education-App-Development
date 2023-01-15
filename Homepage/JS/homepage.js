7fb3ad46294dd5c6929189b721c194b093db38d4
Haris Mohanty <harismohanty8658@gmail.com>
2023-01-15 09:20:04 +0530

    HOMEPAGE CODE :- ( After click the login :- Redirecting to next page code processing..........)

--- Homepage/JS/homepage.js
+++ Homepage/JS/homepage.js
@@ -7,27 +7,51 @@ for (i = 0; i < localStorage.length; i++) {
     allBrandKey.push(allKeys.replace("_brand", ""));
   }
 }
-//GET BRANDCODE FROM LOCALSTORAGE CODE END
+// //GET BRANDCODE FROM LOCALSTORAGE CODE END
 
-//SHOW BRANDCODE IN "CHOOSE BRAND CODE" OPTION CODE START
+// //SHOW BRANDCODE IN "CHOOSE BRAND CODE" OPTION CODE START
 let brandCode_El = document.querySelector("#brand-code-el");
 allBrandKey.forEach((code, index) => {
   brandCode_El.innerHTML += `
     <option value="${code}">${code}</option>
     `;
 });
-//SHOW BRANDCODE IN "CHOOSE BRAND CODE" OPTION CODE END
+// //SHOW BRANDCODE IN "CHOOSE BRAND CODE" OPTION CODE END
 
 //LOGIN CODE START
-let loginForm = document.querySelector(".login-form");
-let allLoginInput = loginForm.querySelectorAll("INPUT");
-let loginBtn = loginForm.querySelector(".login-btn");
-// loginBtn[0].disabled = true;
+// let loginForm = document.querySelector(".login-form");
+let allLoginInput = document.querySelectorAll("INPUT");
+let loginBtn = document.querySelector(".login-btn");
+let brandCode;
+let allUserData = [];
 brandCode_El.addEventListener("change", () => {
-  if(brandCode_El.value != "Choose Brand Code"){
-    
-  }else{
-    swal("Unselected Brandcode!", "Please Select a Brandcode !", "warning")
+  if (brandCode_El.value != "Choose Brand Code") {
+    sessionStorage.setItem("brandcode", brandCode_El.value);
+    allLoginInput[0].disabled = false;
+    allLoginInput[1].disabled = false;
+    loginBtn.disabled = false;
+    brandCode = sessionStorage.getItem("brandcode");
+    loginUserFun();
+  } else {
+    allLoginInput[0].disabled = true;
+    allLoginInput[1].disabled = true;
+    loginBtn.disabled = true;
+    swal("Unselected Brandcode!", "Please Select a Brandcode !", "warning");
   }
 });
-//LOGIN CODE END
+
+const loginUserFun = () => {
+  if(localStorage.getItem(brandCode+"_registrationData") != null){
+    allUserData = JSON.parse(localStorage.getItem(brandCode + "_registrationData"));
+  }
+  loginBtn.onclick = function(){
+    for(i=0; i<allUserData.length; i++){
+      if(allUserData[i].enrollment == allLoginInput[0].value){
+
+      }else{
+        
+      }
+    }
+  }
+};
+// // //LOGIN CODE END
