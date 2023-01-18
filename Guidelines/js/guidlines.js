7ba792c8ae27224f4eed9832c9fa5b3b8fff525c
Haris Mohanty <harismohanty8658@gmail.com>
2023-01-18 20:28:08 +0530

    GUIDLINES Page :- ( START EXAM BUTTON code processsing......)

--- Guidelines/js/guidlines.js
+++ Guidelines/js/guidlines.js
@@ -1,14 +1,22 @@
 let brandCode = sessionStorage.getItem("brandCode");
 let allSubject = [];
 let selectSubjectEl = document.querySelector("#select-subject");
-console.log(selectSubjectEl)
+console.log(selectSubjectEl);
 //GET SUBJECT FROM LOCALSTORAGE CODE START
 if (localStorage.getItem(brandCode + "_allSubject") != null) {
   allSubject = JSON.parse(localStorage.getItem(brandCode + "_allSubject"));
-  allSubject.forEach((subject,index) => {
+  allSubject.forEach((subject, index) => {
     selectSubjectEl.innerHTML += `
     <option>${subject.subjectName}</option>
     `;
   });
 }
 //GET SUBJECT FROM LOCALSTORAGE CODE END
+
+//START EXAM BUTTON CODE START
+let startExambtn = document.querySelector(".start-exam-btn");
+startExambtn.onclick = function () {
+    alert()
+};
+
+//START EXAM BUTTON CODE END
