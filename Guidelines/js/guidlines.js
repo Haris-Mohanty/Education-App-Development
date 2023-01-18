06903af586bd2b3cc40182bc96a504b5f4b193ec
Haris Mohanty <harismohanty8658@gmail.com>
2023-01-18 20:23:54 +0530

    GUIDLINES Page :- ( CHOOSE SUBJECT :- ( Subject getting from LocalStorage successfully.))

--- Guidelines/js/guidlines.js
+++ Guidelines/js/guidlines.js
@@ -1,8 +1,14 @@
 let brandCode = sessionStorage.getItem("brandCode");
 let allSubject = [];
+let selectSubjectEl = document.querySelector("#select-subject");
+console.log(selectSubjectEl)
 //GET SUBJECT FROM LOCALSTORAGE CODE START
-if(localStorage.getItem(brandCode + "_allSubject") != null){
-    allSubject = JSON.parse(localStorage.getItem(brandCode+"_allSubject"));
-    console.log(allSubject)
+if (localStorage.getItem(brandCode + "_allSubject") != null) {
+  allSubject = JSON.parse(localStorage.getItem(brandCode + "_allSubject"));
+  allSubject.forEach((subject,index) => {
+    selectSubjectEl.innerHTML += `
+    <option>${subject.subjectName}</option>
+    `;
+  });
 }
 //GET SUBJECT FROM LOCALSTORAGE CODE END
