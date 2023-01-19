2c83b47b6b82aa838bab95434d5f71be9a1d9c84
Haris Mohanty <harismohanty8658@gmail.com>
2023-01-19 19:43:05 +0530

    QUESTIONS PAGE :- ( Showing questions in Page successfuly (from localstorage) and show options code processing... )

--- Questions/js/questions.js
+++ Questions/js/questions.js
@@ -3,7 +3,18 @@ let brandCode = sessionStorage.getItem("brandCode");
 let allQuestion = [];
 //GET QUESTION FROM LOCALSTORAGE CODE START
 if (localStorage.getItem(brandCode + "_" + subject + "_question") != null) {
-  allQuestion = JSON.parse(localStorage.getItem(brandCode + "_" + subject + "_question"));
-  console.log(allQuestion);
+  allQuestion = JSON.parse(
+    localStorage.getItem(brandCode + "_" + subject + "_question")
+  );
 }
 //GET QUESTION FROM LOCALSTORAGE CODE END
+
+//SHOWING QUESTION IN PAGE CODE START
+let questionEl = document.querySelector(".question-el");
+let index = 0;
+const getQuestionFun = () => {
+    let data = allQuestion[index];
+    questionEl.innerHTML = `Q.${index+1}: ${data.question}`;
+};
+getQuestionFun();
+//SHOWING QUESTION IN PAGE CODE END
