0c8f8fe9f82bcd31b7480e756375cb96fecefc11
Haris Mohanty <harismohanty8658@gmail.com>
2023-01-19 20:00:21 +0530

    QUESTIONS PAGE :- ( Showing QUESTION AND OPTIONS in page code Fixed. )

--- Questions/js/questions.js
+++ Questions/js/questions.js
@@ -11,10 +11,15 @@ if (localStorage.getItem(brandCode + "_" + subject + "_question") != null) {
 
 //SHOWING QUESTION IN PAGE CODE START
 let questionEl = document.querySelector(".question-el");
+let allOptions = document.querySelectorAll(".option");
 let index = 0;
 const getQuestionFun = () => {
     let data = allQuestion[index];
     questionEl.innerHTML = `Q.${index+1}: ${data.question}`;
+    allOptions[0].nextElementSibling.innerHTML = data.optionOne;
+    allOptions[1].nextElementSibling.innerHTML = data.optionTwo;
+    allOptions[2].nextElementSibling.innerHTML = data.optionThree;
+    allOptions[3].nextElementSibling.innerHTML = data.optionFour;
 };
 getQuestionFun();
 //SHOWING QUESTION IN PAGE CODE END
